d3098583a401c31a6e75600421ae7d0909105463
OpenSoftware-World <143949134+OpenSoftware-World@users.noreply.github.com>
2026-05-23 19:47:29 +0300

    Language support has been added for KedyBox. (English / Turkish)

--- build.py
+++ build.py
@@ -3,103 +3,106 @@ import os
 import glob
 import sys
 import time
+import platform
+from Lang.lang import *
 
-init(autoreset=True)
+if platform.system() == "Linux":
+    init(autoreset=True)
 
-unzip_loc = "/usr/bin/unzip"
-sysroot_loc = os.path.abspath("rootfs/usr")
-gcc_loc = "/usr/bin/aarch64-linux-gnu-gcc"
+    unzip_loc = "/usr/bin/unzip"
+    sysroot_loc = os.path.abspath("rootfs/usr")
+    gcc_loc = "/usr/bin/aarch64-linux-gnu-gcc"
 
-print(Fore.RED + "KedyBox - PatiOS ENV Builder by mehmetdemir-tr")
+    print(Fore.RED + "KedyBox - PatiOS ENV Builder by mehmetdemir-tr")
 
+    def extract():
+        print(Fore.GREEN + fs_unzip_content)
 
-def extract():
-    print(Fore.GREEN + "Dosya sistemi çıkarılıyor..")
+        if not os.path.exists(unzip_loc):
+            print(Fore.YELLOW + unzip_not_found_error)
+            ret = os.system("sudo apt install unzip -y")
+            if ret != 0:
+                print(Fore.RED + unzip_not_setup_error)
+                sys.exit(1)
 
-    if not os.path.exists(unzip_loc):
-        print(Fore.YELLOW + "[!] unzip bulunamadı, kuruluyor...")
-        ret = os.system("sudo apt install unzip -y")
-        if ret != 0:
-            print(Fore.RED + "[-] unzip kurulamadı, çıkılıyor.")
+        ret1 = os.system(f"{unzip_loc} paticommands.zip -d pati-commands/")
+        ret2 = os.system(f"{unzip_loc} filesystem.zip -d rootfs/")
+
+        if ret1 != 0 or ret2 != 0:
+            print(Fore.RED + unzip_not_success_error)
             sys.exit(1)
 
-    ret1 = os.system(f"{unzip_loc} paticommands.zip -d pati-commands/")
-    ret2 = os.system(f"{unzip_loc} filesystem.zip -d rootfs/")
-
-    if ret1 != 0 or ret2 != 0:
-        print(Fore.RED + "[-] ZIP çıkartma başarısız, çıkılıyor.")
-        sys.exit(1)
-
-    print(Fore.GREEN + "[+] Dosya sistemi hazır.")
+        print(Fore.GREEN + fs_success)
 
 
-def compile():
-    if not os.path.exists(gcc_loc):
-        print(Fore.YELLOW + "[!] aarch64-gcc bulunamadı, kuruluyor...")
-        ret = os.system("sudo apt install gcc-aarch64-linux-gnu binutils-aarch64-linux-gnu -y")
-        if ret != 0:
-            print(Fore.RED + "[-] GCC kurulamadı, çıkılıyor.")
+    def compile():
+        if not os.path.exists(gcc_loc):
+            print(Fore.YELLOW + aarch64_gcc_not_found_error)
+            ret = os.system("sudo apt install gcc-aarch64-linux-gnu binutils-aarch64-linux-gnu -y")
+            if ret != 0:
+                print(Fore.RED + gcc_not_setup_error)
+                sys.exit(1)
+
+        dosyalar = glob.glob("pati-commands/*.c")
+        if not dosyalar:
+            print(Fore.RED + pati_commands_file_not_found_error)
             sys.exit(1)
 
-    dosyalar = glob.glob("pati-commands/*.c")
-    if not dosyalar:
-        print(Fore.RED + "[-] pati-commands/ içinde .c dosyası bulunamadı.")
-        sys.exit(1)
+        sysroot = os.path.abspath("rootfs/usr")
+        flags = f"--sysroot={sysroot} -I{sysroot}/include -I/usr/include -L{sysroot}/lib -static"
 
-    sysroot = os.path.abspath("rootfs/usr")
-    flags = f"--sysroot={sysroot} -I{sysroot}/include -I/usr/include -L{sysroot}/lib -static"
+        basari = 0
+        hata = 0
+        for f in dosyalar:
+            out = f.replace(".c", "")
+            if "mauvyd.c" in f:
+                ret = os.system(f"{gcc_loc} {flags} -Ipati-commands/pati-headers {f} pati-commands/pati-headers/pcg.c -o {out}")
+            else:
+                ret = os.system(f"{gcc_loc} {flags} {f} -o {out}")
+            if ret == 0:
+                print(Fore.GREEN + f"{compile_success} {f} -> {out}")
+                basari += 1
+            else:
+                print(Fore.RED + f"{compile_not_success} {f}")
+                hata += 1
+        print(Fore.GREEN + f"{compilation_complete} {basari} {success}, {hata} {incorrect}")
 
-    basari = 0
-    hata = 0
-    for f in dosyalar:
-        out = f.replace(".c", "")
-        if "mauvyd.c" in f:
-            ret = os.system(f"{gcc_loc} {flags} -Ipati-commands/pati-headers {f} pati-commands/pati-headers/pcg.c -o {out}")
-        else:
-            ret = os.system(f"{gcc_loc} {flags} {f} -o {out}")
+
+    def create_nodes():
+        print(Fore.GREEN + create_dev_nodes)
+        os.system("sudo rm -f rootfs/dev/vda rootfs/dev/fb0 rootfs/dev/rtc0 rootfs/dev/urandom")
+        os.system("sudo mknod rootfs/dev/vda b 254 0")
+        os.system("sudo mknod rootfs/dev/fb0 c 29 0")
+        os.system("sudo mknod rootfs/dev/rtc0 c 253 0")
+        os.system("sudo mknod rootfs/dev/urandom c 1 9")
+
+    def move():
+        os.makedirs("rootfs/lib/paticommands", exist_ok=True)
+
+        if os.path.exists("pati-commands/mauvyd"):
+            os.system("cp pati-commands/mauvyd rootfs/init")
+            os.system("rm -f pati-commands/mauvyd")
+        if os.path.exists("pati-commands/shell"):
+            os.system("cp pati-commands/shell rootfs/bin/")
+            os.system("rm -f pati-commands/shell")
+
+        for f in glob.glob("pati-commands/*"):
+            if not f.endswith(".c") and os.path.isfile(f):
+                os.system(f"cp {f} rootfs/lib/paticommands/")
+                print(Fore.GREEN + f"{moved} {f}")
+                os.system(f"rm -f {f}")
+        os.system("rm -f rootfs/lib/paticommands/init rootfs/lib/paticommands/shell")
+        print(Fore.GREEN + create_initramfs)
+        ret = os.system("cd rootfs && find . | cpio -o -H newc | gzip -9 > ../initramfs.cpio.gz")
         if ret == 0:
-            print(Fore.GREEN + f"[+] Derlendi: {f} -> {out}")
-            basari += 1
+            print(Fore.GREEN + initramfs_ready)
         else:
-            print(Fore.RED + f"[-] Hata: {f}")
-            hata += 1
-    print(Fore.GREEN + f"[+] Derleme tamam: {basari} başarılı, {hata} hatalı.")
+            print(Fore.RED + not_create_initramfs)
+            sys.exit(1)
 
-
-def create_nodes():
-    print(Fore.GREEN + "[+] Device nodes oluşturuluyor...")
-    os.system("sudo rm -f rootfs/dev/vda rootfs/dev/fb0 rootfs/dev/rtc0 rootfs/dev/urandom")
-    os.system("sudo mknod rootfs/dev/vda b 254 0")
-    os.system("sudo mknod rootfs/dev/fb0 c 29 0")
-    os.system("sudo mknod rootfs/dev/rtc0 c 253 0")
-    os.system("sudo mknod rootfs/dev/urandom c 1 9")
-
-def move():
-    os.makedirs("rootfs/lib/paticommands", exist_ok=True)
-
-    if os.path.exists("pati-commands/mauvyd"):
-        os.system("cp pati-commands/mauvyd rootfs/init")
-        os.system("rm -f pati-commands/mauvyd")
-    if os.path.exists("pati-commands/shell"):
-        os.system("cp pati-commands/shell rootfs/bin/")
-        os.system("rm -f pati-commands/shell")
-
-    for f in glob.glob("pati-commands/*"):
-        if not f.endswith(".c") and os.path.isfile(f):
-            os.system(f"cp {f} rootfs/lib/paticommands/")
-            print(Fore.GREEN + f"[+] Taşındı: {f}")
-            os.system(f"rm -f {f}")
-    os.system("rm -f rootfs/lib/paticommands/init rootfs/lib/paticommands/shell")
-    print(Fore.GREEN + "[+] initramfs oluşturuluyor...")
-    ret = os.system("cd rootfs && find . | cpio -o -H newc | gzip -9 > ../initramfs.cpio.gz")
-    if ret == 0:
-        print(Fore.GREEN + "[+] initramfs.cpio.gz hazır.")
-    else:
-        print(Fore.RED + "[-] initramfs oluşturulamadı.")
-        sys.exit(1)
-
-
-extract()
-compile()
-create_nodes()
-move()
+    extract()
+    compile()
+    create_nodes()
+    move()
+else:
+    print("You cannot run this script on your system. Please make sure you are using a Linux-based operating system")
